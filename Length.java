d33574552b18c61d30bb2456e3a3ed58ec77665a
zlpnprdnxx <afahiyah105@gmail.com>
2024-11-16 13:11:29 +0700

    Add files via upload

---
+++ Length.java
@@ -0,0 +1,11 @@
+public class Main {
+ public static void main(String[] args) {
+ String firstName = "Farid Abdul";
+ String lastName = "Rozak";
+ 
+ System.out.println("Nama Pertama : "+firstName);
+System.out.println("Nama Kedua : "+lastName);
+ System.out.println("Nama Panjang Pertama : "+firstName.length());
+ System.out.println("Nama Panjang Kedua : "+lastName.length());
+    }
+}
